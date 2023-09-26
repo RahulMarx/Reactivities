494d749f7d66ff8afa718d51873df227a18c10b1
RahulMarx <rahul707mukherjee@gmail.com>
2023-09-27 00:17:41 +0530

    Initial Commit

---
+++ Persistence/Migrations/20230826235744_UpdateActivityClass.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateActivityClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descrription",
+                table: "Activities",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Activities",
+                newName: "Descrription");
+        }
+    }
+}
